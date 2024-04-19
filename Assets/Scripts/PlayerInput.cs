7a06ac6f134546f1ff797aa5dd875d0e66c62b31
Kacper Mejsner <kacpermejs@gmail.com>
2024-04-19 22:43:18 +0200

    Player controller

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
+
+[RequireComponent(typeof(SubmarineController))]
+public class PlayerInput : MonoBehaviour {
+
+  [SerializeField]
+  private float _tiltInputFactor = 1f;
+  [SerializeField]
+  private float _elevationInputFactor = 1f;
+  [SerializeField]
+  private float _throttleInputFactor = 1f;
+  [SerializeField]
+  private float[] _steps = { -1f, -0.33f, 0f, 0.33f, 1f };
+  [SerializeField]
+  private int _initialStep = 2;
+
+  private int _currentStep;
+  private float _inputStep = 0f;
+
+
+  [SerializeField]
+  private InputActionReference throttle;
+  [SerializeField]
+  private InputActionReference tilt;
+  [SerializeField]
+  private InputActionReference elevation;
+
+  private SubmarineController controller;
+
+  private void Awake() {
+    controller = GetComponent<SubmarineController>();
+    _currentStep = _initialStep;
+  }
+
+  private void Update() {
+    //throttle
+    UpdateThrottle();
+    UpdateBuoyancy();
+    UpdateBuoyancyDisproportion();
+  }
+
+  private void UpdateBuoyancy() {
+    float value = elevation.action.ReadValue<float>();
+    if (value > 0.01 || value < -0.01) {
+      //steering up means the water weight has to go down for submarine to go up
+      controller.UpdateBuoyancy(_elevationInputFactor * -value * Time.deltaTime);
+    }
+  }
+  
+  private void UpdateBuoyancyDisproportion() {
+    float value = tilt.action.ReadValue<float>();
+    if (value > 0.01 || value < -0.01) {
+      controller.Tilt(_tiltInputFactor * value * Time.deltaTime);
+    }
+  }
+
+  private void UpdateThrottle() {
+    float value = throttle.action.ReadValue<float>();
+    if (value < -0.01 || value > 0.01) {
+      _inputStep += _throttleInputFactor * Time.deltaTime;
+      if (_inputStep > 1f) {
+        if (value > 0f) {
+          StepUpThrottle();
+          _inputStep = 0;
+        } else {
+          StepDownThrottle();
+          _inputStep = 0;
+        }
+      }
+    } else {
+      _inputStep = 0;
+    }
+  }
+
+  void StepUpThrottle() {
+    if (_currentStep < _steps.Length - 1) {
+      _currentStep++;
+      controller.UpdateThrottle(_steps[_currentStep]);
+    }
+  }
+
+  void StepDownThrottle() {
+    if (_currentStep > 0) {
+      _currentStep--;
+      controller.UpdateThrottle(_steps[_currentStep]);
+    }
+  }
+
+
+}
+
---
+++ Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class SubmarineController : MonoBehaviour {
+
+  [SerializeField] private float _defaultBuoyancy;
+  [SerializeField] private float _buoyancyForceMagnitude;
+  [SerializeField] private float _throttleForceMagnitude;
+
+  [Range(-1f, 1f)]
+  [SerializeField]
+  private float _throttle;
+
+  [Range(0f, 1f)]
+  [SerializeField]
+  private float _frontTankFill;
+
+  [Range(0f, 1f)]
+  [SerializeField]
+  private float _rearTankFill;
+
+  [SerializeField]
+  private Transform _frontTankTransform;
+
+  [SerializeField]
+  private Transform _rearTankTransform;
+
+  private Rigidbody2D _rb;
+
+  private void Awake() {
+    _rb = GetComponent<Rigidbody2D>();
+  }
+
+  private void FixedUpdate() {
+
+    //buoyancy
+    _rb.AddForce(Vector2.up * _defaultBuoyancy);
+
+    //front
+    _rb.AddForceAtPosition(Vector2.up * _buoyancyForceMagnitude * -_frontTankFill, _frontTankTransform.position);
+
+    //back
+    _rb.AddForceAtPosition(Vector2.up * _buoyancyForceMagnitude * -_rearTankFill, _rearTankTransform.position);
+
+    //throttle
+    _rb.AddForce(Vector2.right * _throttle * _throttleForceMagnitude);
+
+  }
+
+  public void UpdateThrottle(float value) {
+    _throttle = Mathf.Clamp(value, -1, 1);
+  }
+
+  public void UpdateBuoyancy(float offset) {
+    _frontTankFill = Mathf.Clamp01(_frontTankFill + offset);
+    _rearTankFill = Mathf.Clamp01(_rearTankFill + offset);
+  }
+
+  public void Tilt(float offset) {
+    _frontTankFill = Mathf.Clamp01(_frontTankFill + offset);
+    _rearTankFill = Mathf.Clamp01(_rearTankFill - offset);
+  }
+
+  public void Level() {
+    float combinedFill = (_frontTankFill + _rearTankFill) / 2f;
+    _frontTankFill = combinedFill;
+    _rearTankFill = combinedFill;
+  }
+}
